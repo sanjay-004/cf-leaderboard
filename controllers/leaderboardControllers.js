63e24e0b249d223b1fbd3ee8b7c8d51cc46e513f
sanjay-004 <106620128+sanjay-004@users.noreply.github.com>
2023-08-01 14:06:44 +0530

    Update leaderboardControllers.js

--- controllers/leaderboardControllers.js
+++ controllers/leaderboardControllers.js
@@ -1,30 +1,16 @@
 const asyncHandler = require("express-async-handler");
 const user = require("../models/userModel");
 const https = require('https');
+const axios = require('axios');
 
-function getResponse(url) {
-    return new Promise((resolve, reject) => {
-        https.get(url, (res) => {
-          let body = "";
-    
-          res.on("data", (chunk) => {
-            body += chunk;
-          });
-    
-          res.on("end", () => {
-            try {
-              const response = JSON.parse(body);
-              resolve(response);
-            } catch (error) {
-              console.error(error.message);
-              reject(error);
-            }
-          });
-        }).on("error", (error) => {
-          console.error(error.message);
-          reject(error);
-        });
-    });
+async function getResponse(url) {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error(error.message);
+    throw error;
+  }
 }
 
 const getLeaderboard = asyncHandler (async (req, res) => {
